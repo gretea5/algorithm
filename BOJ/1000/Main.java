dac8aa99381d070d2034ffbf1bd1868ba478b8c4
Gretea5 <120379834+Gretea5@users.noreply.github.com>
2022-12-13 16:14:13 +0900

    Create Main.java

---
+++ BOJ/1000/Main.java
@@ -0,0 +1,14 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.print(A + B);
+    }
+}
