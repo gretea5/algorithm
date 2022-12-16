62cc2eb5bbc408f3a72b4d526851351973add135
gretea5 <120379834+gretea5@users.noreply.github.com>
2022-12-16 15:01:35 +0900

    Create Main.java

---
+++ BOJ/5585/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static int totalMoney = 1000;
+    public static int moneyArr[] = {500, 100, 50, 10, 5, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int payMoney = Integer.parseInt(br.readLine());
+        int restMoney = totalMoney - payMoney;
+        int totalcount = 0;
+        for(int i = 0; i < moneyArr.length; i++) {
+            int count = (restMoney / moneyArr[i]);
+            totalcount += (restMoney / moneyArr[i]);
+            restMoney -= (moneyArr[i] * count);
+        }
+        System.out.print(totalcount);
+        br.close();
+    }
+}
