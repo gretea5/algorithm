a67f857d935b368a75d2d4e77faa2982f26e0b23
gretea5 <120379834+gretea5@users.noreply.github.com>
2022-12-17 22:44:57 +0900

    Create Main.java

---
+++ BOJ/13305/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long distanceArr[] = new long[N - 1];
+        long oilUnitArr[] = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        //두 도시간의 거리를 입력
+        for(int i = 0; i < N - 1; i++) {
+            distanceArr[i] = Long.parseLong(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine(), " ");
+        //도시의 기름 단위를 입력
+        for(int i = 0; i < N; i++) {
+            oilUnitArr[i] = Long.parseLong(st.nextToken());
+        }
+        long result = 0;
+        long min = oilUnitArr[0];
+        for(int i = 1; i < N; i++) {
+            result += (min * distanceArr[i - 1]);
+            if (min > oilUnitArr[i]) {
+                min = oilUnitArr[i];
+            }
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
