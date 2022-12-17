b386bfdf9738af16da238ee77bcee4e347fc1b32
gretea5 <120379834+gretea5@users.noreply.github.com>
2022-12-17 19:54:47 +0900

    Create Main.java

---
+++ BOJ/10818/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        int min = Collections.min(list);
+        int max = Collections.max(list);
+        System.out.print(min + " " + max);
+        br.close();
+    }
+}
