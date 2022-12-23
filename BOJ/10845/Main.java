e5507c66255531a7001912f68812e18be9e4ad07
gretea5 <120379834+gretea5@users.noreply.github.com>
2022-12-23 13:01:44 +0900

    Create Main.java

---
+++ BOJ/10845/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String command = st.nextToken();
+            if(command.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                deque.addFirst(num);
+            } else if (command.equals("pop")) {
+                System.out.println((deque.isEmpty() ? -1 : deque.pollLast()));
+            } else if (command.equals("size")) {
+                System.out.println(deque.size());
+            } else if (command.equals("empty")) {
+                System.out.println((deque.isEmpty() ? 1 : 0));
+            } else if (command.equals("front")) {
+                System.out.println((deque.isEmpty() ? -1 : deque.peekLast()));
+            } else if (command.equals("back")) {
+                System.out.println((deque.isEmpty() ? -1 : deque.peekFirst()));
+            }
+        }
+        br.close();
+    }
+}
