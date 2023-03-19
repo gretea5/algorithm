91f6666c6aa4711bb82e0e15d97882957224ba63
JangHoon Park <120379834+gretea5@users.noreply.github.com>
2023-03-19 13:56:43 +0900

    2747 피보나치 수

---
+++ BOJ/2747/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[46];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= 45; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.print(dp[n]);
+        br.close();
+    }
+}
