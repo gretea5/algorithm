e53f0c4c5dbd58ae19e1925999614a8097f5b618
JangHoon Park <120379834+gretea5@users.noreply.github.com>
2023-03-22 23:08:58 +0900

    15486 퇴사 2

---
+++ BOJ/15486/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N + 2];
+        int[] P = new int[N + 2];
+        int[] dp = new int[N + 2];
+        for(int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i <= N + 1; i++) {
+            max = Math.max(max, dp[i]);
+            if (i + T[i] <= N + 1) {
+                dp[i + T[i]] = Math.max(dp[i + T[i]], max + P[i]);
+            }
+        }
+        System.out.print(max);
+        br.close();
+    }
+}
