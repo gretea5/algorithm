6ab3d6302e9b52bd2434b20199868e03bd215fb6
JangHoon Park <120379834+gretea5@users.noreply.github.com>
2023-08-08 13:22:13 +0900

    11478 서로 다른 부분 문자열의 개수

---
+++ BOJ/11478/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashSet;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        HashSet<String> set = new HashSet<>();
+        for(int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                set.add(input.substring(i, j));
+            }
+        }
+        System.out.print(set.size());
+        br.close();
+    }
+}
