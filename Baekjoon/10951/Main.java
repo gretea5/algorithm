aacec39154979520ffbe9c3dc0035343a171b21f
JangHoon Park <120379834+gretea5@users.noreply.github.com>
2024-01-20 11:01:45 +0900

    10951 A+B - 4

---
+++ Baekjoon/10951/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        String input;
+        while((input = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(input, " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(A + B).append("\n");
+        }
+
+        System.out.print(sb);
+
+        br.close();
+    }
+}
