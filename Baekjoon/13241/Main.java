e1bb5e35de89fe2b9eb862b771d40245d5a3e925
JangHoon Park <120379834+gretea5@users.noreply.github.com>
2024-02-11 16:41:07 +0900

    13241 최소공배수

---
+++ Baekjoon/13241/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        BigInteger G = A.gcd(B);
+
+        System.out.print(A.multiply(B).divide(G).longValue());
+
+        br.close();
+    }
+}
