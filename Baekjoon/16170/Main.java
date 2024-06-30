8c1857ed57d21ca6616f9e5798c3ca042928f683
Janghoon Park <gretea4489@gmail.com>
2024-06-30 21:00:41 +0900

    16170 오늘의 날짜는?

---
+++ Baekjoon/16170/Main.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+
+        now.minusHours(9);
+
+        System.out.println(now.getYear());
+        System.out.println(now.getMonthValue());
+        System.out.print(now.getDayOfMonth());
+    }
+}
