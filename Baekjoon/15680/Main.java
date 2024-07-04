b7252efcb54c86c9bfc9cc35e2ad83544e5de4bb
Janghoon Park <gretea4489@gmail.com>
2024-07-05 08:03:12 +0900

    15680 연세대학교

---
+++ Baekjoon/15680/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        System.out.print(n == 0 ? "YONSEI" : "Leading the Way to the Future");
+        
+        br.close();
+    }
+}
