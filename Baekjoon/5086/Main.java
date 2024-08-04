f010a5c06ee1249ee4aa20c08bcdd1174a6bb97c
Janghoon Park <gretea4489@gmail.com>
2024-08-04 16:29:10 +0900

    5086 배수와 약수

---
+++ Baekjoon/5086/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+        
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if (a == 0 && b == 0) break;
+            
+            if (b % a == 0) {
+                sb.append("factor");
+            }
+            else if (a % b == 0) {
+                sb.append("multiple");
+            }
+            else {
+                sb.append("neither");
+            }
+            
+            sb.append("\n");
+        }
+        
+        System.out.print(sb);
+        
+        br.close(); 
+    }
+}
