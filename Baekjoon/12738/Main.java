e744445f060f49c31b68dced64729fba14b89006
Janghoon Park <gretea4489@gmail.com>
2024-08-22 21:56:15 +0900

    12738 가장 긴 증가하는 부분 수열 3

---
+++ Baekjoon/12738/Main.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        arr.add(Integer.MIN_VALUE);
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            
+            if (arr.get(arr.size() - 1) < value) {
+                arr.add(value);
+            }
+            else if (arr.get(arr.size() - 1) > value) {
+                int left = 0;
+                int right = arr.size() - 1;
+                
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+                    
+                    if (value <= arr.get(mid)) {
+                        right = mid - 1;
+                    }
+                    else {
+                        left = mid + 1;
+                    }
+                }
+                
+                arr.set(left, value);
+            }
+        }
+        
+        System.out.print(arr.size() - 1);
+        
+        br.close();
+    }
+}
