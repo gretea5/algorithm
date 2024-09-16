89a89c194f1943849a03acea06954b028a694de9
Janghoon Park <gretea4489@gmail.com>
2024-09-16 15:45:09 +0900

    1038 감소하는 수

--- Baekjoon/1038/Main.java
+++ Baekjoon/1038/Main.java
@@ -5,39 +5,43 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-    private static ArrayList<Long> arr;
+    private static ArrayList<Long> list;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        arr = new ArrayList<>();
-
-        for(int i = 0; i < 10; i++) {
-            backTrack(i, 0);
-        }
-
-        Collections.sort(arr);
-
-        if(arr.size() <= N) {
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        //감소하는 수를 담은 리스트
+        list = new ArrayList<>();
+        
+        backTrack("", 9, 0);
+        
+        if (list.size() < n + 1) {
             System.out.print(-1);
             return;
         }
-
-        System.out.print(arr.get(N));
-
+        
+        //오름차순 정렬
+        Collections.sort(list);
+        
+        //출력
+        System.out.print(list.get(n));
+        
         br.close();
     }
-
-    private static void backTrack(long num, int depth) {
-        if(depth > 10) {
+    
+    private static void backTrack(String s, int idx, int depth) {
+        if (depth > 10) {
             return;
         }
-
-        arr.add(num);
-
-        for(int i = 0; i < (num % 10); i++) {
-            backTrack((num * 10) + i, depth + 1);
+        
+        if (!s.isEmpty()) {
+            list.add(Long.parseLong(s));
+        }
+        
+        for (int i = idx; i >= 0; i--) {
+            backTrack(s + i, i - 1, depth + 1);
         }
     }
 }
