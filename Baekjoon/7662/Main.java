016d00f50839c3b5863adc13e5b1065e11ade860
Janghoon Park <gretea4489@gmail.com>
2024-09-20 22:56:51 +0900

    7662 이중 우선순위 큐

--- Baekjoon/7662/Main.java
+++ Baekjoon/7662/Main.java
@@ -1,75 +1,77 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.TreeMap;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int test = 1; test <= t; test++) {
-            int k = Integer.parseInt(br.readLine());
-
-            TreeMap<Integer, Integer> map = new TreeMap<>();
-
-            for (int i = 0; i < k; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-                char ch = st.nextToken().charAt(0);
-                int n = Integer.parseInt(st.nextToken());
-
-                if (ch == 'I') {
-                    map.put(n, map.getOrDefault(n, 0) + 1);
-                }
-                else {
-                    if (map.isEmpty()) {
-                        continue;
-                    }
-
-                    //최댓값을 삭제하는 연산
-                    if (n == 1) {
-                        int max = map.lastKey();
-                        int value = map.get(max);
-
-                        if (value == 1) {
-                            map.remove(max);
-                        }
-                        else {
-                            map.put(max, value - 1);
-                        }
-                    }
-                    //최솟값을 삭제하는 연산
-                    else {
-                        int min = map.firstKey();
-                        int value = map.get(min);
-
-                        if (value == 1) {
-                            map.remove(min);
-                        }
-                        else {
-                            map.put(min, value - 1);
-                        }
-                    }
-
-                }
-            }
-
-            if (map.isEmpty()) {
-                sb.append("EMPTY").append("\n");
-                continue;
-            }
-
-            sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
-        }
-
-        System.out.print(sb);
-
-
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int test = 1; test <= t; test++) {
+		    int k = Integer.parseInt(br.readLine());
+		    
+		    TreeMap<Integer, Integer> map = new TreeMap<>();
+		    
+		    while (k > 0) {
+		        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		        
+		        String command = st.nextToken();
+		        int n = Integer.parseInt(st.nextToken());
+		        
+		        if (command.equals("I")) {
+		            map.put(n, map.getOrDefault(n, 0) + 1);
+		        }
+		        else {
+		            if (!map.keySet().isEmpty()) {
+		                //최댓값
+    		            if (n == 1) {
+    		                int key = map.lastKey();
+    		                
+    		                int value = map.get(key);
+    		                
+    		                if (value == 1) {
+    		                    map.remove(key);
+    		                }
+    		                else {
+    		                    map.put(key, value - 1);
+    		                }
+    		            }
+    		            //최솟값
+    		            else {
+    		               int key = map.firstKey();
+    		               
+    		               int value = map.get(key);
+    		                
+        	                if (value == 1) {
+        	                    map.remove(key);
+        	                }
+        	                else {
+        	                    map.put(key, value - 1);
+        	                }
+    		            }
+		            }
+		        }
+		        
+		        k -= 1;
+		    }
+		    
+		    if (map.keySet().isEmpty()) {
+		        sb.append("EMPTY");
+		    }
+		    else {
+		        sb.append(map.lastKey()).append(" ").append(map.firstKey());
+		    }
+		    
+		    sb.append("\n");
+		}
+		
+		System.out.print(sb);
+		
+		br.close();
+	}
 }
