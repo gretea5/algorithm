92753e7cf06bde89ab7a97d4c5a44b7ad5fdd7fa
Janghoon Park <gretea4489@gmail.com>
2024-09-23 23:22:25 +0900

    9295 주사위

---
+++ Baekjoon/9295/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int test = 1; test <= t; test++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case ").append(test).append(": ");
+            sb.append(a + b).append("\n");
+        }
+
+        System.out.print(sb);
+
+        br.close();
+    }
+}
