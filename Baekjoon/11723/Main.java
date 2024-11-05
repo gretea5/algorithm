b9b1e0b76c5ea6d0cbb64718abf27f781df21b1c
Janghoon Park <gretea4489@gmail.com>
2024-11-05 12:59:38 +0900

    11723 집합

--- Baekjoon/11723/Main.java
+++ Baekjoon/11723/Main.java
@@ -2,55 +2,54 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.HashSet;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-
-        HashSet<Integer> set = new HashSet<>();
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int test = 1; test <= T; test++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-            String command = st.nextToken();
-
-            if(command.equals("all")) {
-                set.clear();
-
-                for(int i = 1; i <= 20; i++) {
-                    set.add(i);
-                }
-            }
-            else if(command.equals("empty")) {
-                set.clear();
-            }
-            else if(command.equals("add")) {
-                set.add(Integer.parseInt(st.nextToken()));
-            }
-            else if(command.equals("remove")) {
-                set.remove(Integer.parseInt(st.nextToken()));
-            }
-            else if(command.equals("check")) {
-                sb.append(set.contains(Integer.parseInt(st.nextToken())) ? 1 : 0).append("\n");
-            }
-            else {
-                int x = Integer.parseInt(st.nextToken());
-                if(set.contains(x)) {
-                    set.remove(x);
-                }
-                else {
-                    set.add(x);
-                }
-            }
-        }
-
-        System.out.print(sb);
-
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int m = Integer.parseInt(br.readLine());
+		
+		int total = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while (m > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			String command = st.nextToken();
+			
+			if ("add".equals(command)) {
+				int x = Integer.parseInt(st.nextToken());
+				
+				total = (total | (1 << (x - 1)));
+			}
+			else if ("remove".equals(command)) {
+				int x = Integer.parseInt(st.nextToken());
+				
+				total = (total & ~(1 << (x - 1)));
+			}
+			else if ("check".equals(command)) {
+				int x = Integer.parseInt(st.nextToken());
+				
+				sb.append((total & (1 << (x - 1))) != 0 ? 1 : 0).append("\n");
+			}
+			else if ("toggle".equals(command)) {
+				int x = Integer.parseInt(st.nextToken());
+				
+				total = ((1 << (x - 1)) ^ total);
+			}
+			else if ("all".equals(command)) {
+				total = (1 << 20) - 1;
+			}
+			else {
+				total = 0;
+			}
+			
+			m -= 1;
+		}
+		
+		System.out.println(sb);
+		
+		br.close();
+	}
 }
