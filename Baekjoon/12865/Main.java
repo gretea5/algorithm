71bc2344f541d4b0b324122a445ad7dee627a88e
Janghoon Park <gretea4489@gmail.com>
2024-12-06 15:48:02 +0900

    [백준] 12865 평범한 배낭

--- Baekjoon/12865/Main.java
+++ Baekjoon/12865/Main.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class Good {
-        int w;
-        int v;
+    private static class Thing {
+        int weight;
+        int value;
 
-        Good(int w, int v) {
-            this.w = w;
-            this.v = v;
+        Thing(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
         }
     }
 
@@ -19,39 +19,42 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        Good[] goods = new Good[N + 1];
+        Thing[] arr = new Thing[n + 1];
 
-        for(int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
 
             int w = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
 
-            goods[i] = new Good(w, v);
+            arr[i] = new Thing(w, v);
         }
 
-        int[][] dp = new int[N + 1][K + 1];
+        //[i][j] => i번째 물건을 담았을때, 최대 j만큼의 무게를 넣었을때 최대 가치
+        int[][] dp = new int[n + 1][k + 1];
 
-        for(int i = 1; i <= N; i++) {
-            int weight = goods[i].w;
-            int value = goods[i].v;
+        for (int i = 1; i <= n; i++) {
+            //현재 물건을 가져옴
+            Thing now = arr[i];
 
-            for(int j = 1; j <= K; j++) {
-                //i - 1번째까지에서 j무게의 최대 가치를 가져온다.
+            for (int j = 1; j <= k; j++) {
+                //현재 물건을 반영하지 않고의, 최대 가치를 가져옴
                 dp[i][j] = dp[i - 1][j];
 
-                //j가 현재 무게보다 작다면, 즉 여분의 무게가 있다면,
-                if(j >= weight) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - weight] + value);
+                //현재 물건을 배낭에 담을 수 있다면,
+                if (j >= now.weight) {
+                    //i - 1번째 물건을 반영했을때, 현재 물건의 무게를 확보한 가치와 물건의 가치를 더해서 비교
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - now.weight] + now.value);
                 }
             }
         }
 
-        System.out.print(dp[N][K]);
-        
+        //n번째 물건을 반영했을때, 최대 k의 무게를 가지는 가방에 넣었을때 최대 가치 출력
+        System.out.print(dp[n][k]);
+
         br.close();
     }
 }
